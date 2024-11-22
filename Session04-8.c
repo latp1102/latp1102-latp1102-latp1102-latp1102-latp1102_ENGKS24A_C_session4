3621f96d5d2c457b8cc83b4e67f25a1da8dca677
latp1102 <latp1102@gmail.com>
2024-11-22 18:06:00 +0700

    Session04 - 8

---
+++ Session04-8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	float a, b, c;
+	
+	printf("Moi ban nhap vao 3 canh cua tam giac");
+	scanf("%f %f %f", &a, &b, &c);
+	
+	if(a > 0 && b >0 && c> 0&& a + b > 0 && a +c > 0 && b + c>0){
+		printf("La 3 canh cua tam giac.\n");
+	} else {
+		printf("Khong phai 3 can cua tam giac.\n");
+	}
+	return 0;
+}
